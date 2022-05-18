b904e33c6e7f12f2e6ebe3e29fd160992941294f
An75k64 <89681244+An75k64@users.noreply.github.com>
2022-05-18 15:02:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello Everyone</h1>
+     <Header/>
+     <Header/>
+     <Header/>
+    </div>
+  );
+}
+
+export default App;
